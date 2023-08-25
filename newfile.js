2b85e0502309e20babbf6ec3259bc51a2057a4f8
Ruben Dietl <u10529@adamhall.com>
2023-08-25 14:01:58 +0200

    removed the unnecessary file

--- newfile.js
+++
@@ -1,9 +0,0 @@
-for (let i = 0; i<=0; i++){
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log("Fizz Buzz");
-    } else if (i % 5 === 0) {
-        console.log("Buzz");
-    } else if (i % 3 === 0) {
-        console.log("Fizz");
-    }
-}
